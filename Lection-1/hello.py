8951bc643c172f8c8519a404b47f33c37ea6cd8d
Daulet Baimbetov <daucloudlab@gmail.com>
2021-09-21 13:07:05 +0600

    Lab-1-ге түсіндірме

--- Lection-1/hello.py
+++ Lection-1/hello.py
@@ -1,3 +1,28 @@
-print("Hello World!")
 
-a = 12
+# for i in range(1, 11):
+#     print()
+
+# i = 1
+# sum = 0
+# while i <= 10:
+#     a = input()
+#     if a < 0:
+#         sum += a
+#     i += 1
+
+# for i in range(3):
+#     for j in range(3):
+#         print('8', end = ' ')
+     
+#     print()
+
+# import random
+
+# random.random()
+# random.choice([3,4, 5,7])
+# random.randint(5, 10)
+# random.randrange(5, 11)
+
+import math
+
+print('{0:.3f}'.format(math.pi) )
