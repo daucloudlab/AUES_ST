cce385fced25d90c170d83f592977c86cffb44d9
Daulet <daucloudlab@gmail.com>
2021-10-07 21:31:39 +0600

    Laboratory work 1, 2 changed

---
+++ Labs/Lab-2/lab2.py
@@ -0,0 +1,69 @@
+# # task 11.2
+# a = []
+# i = 1
+# while i <= 10:
+#     s = int(input("Sandy engiz: "))
+#     a.append(s)
+#     i += 1
+
+# print(a)
+
+
+# # task 11.2
+# a = []
+# i = 1
+# while i <= 10:
+#     a.append(int(input("Sandy engiz: ")))
+#     i += 1
+
+# print(a)
+
+# # task 11.2
+# s = input("Sandy engiz: ")
+# a = s.split()
+# a = [int(i) for i in a]
+
+# # for i in a:
+# #     a.append(int(a))
+    
+# print(a)
+
+# # task 11.3a
+# import random as rn
+# a  = []
+# for i in range(15):
+#     a.append(round(rn.random(), 3))
+# print(a)
+
+
+# # task 11.3b
+# import random as rn
+# a  = []
+# for i in range(15):
+#     a.append(rn.randrange(10))
+# print(a)
+
+
+# # task 12.1
+# first_name = input("First name: ")
+# second_name = input("Second name: ")
+# name  = first_name + second_name
+# print(name)
+
+# task 12.2
+country = input("Country: ")
+city = input("City: ")
+print("The capital of the {0} is {1}".format(country, city))
+print(f"The capital of the {country} is {city}")
+print("The capital of the " + country + " is " +  city)
+
+
+
+
+
+
+
+
+
+
+
